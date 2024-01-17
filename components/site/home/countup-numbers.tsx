b1adec5d2a252e1b364bffde8d0b34afc9aaea6a
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-01-17 15:00:24 -0300

    fix width and add delay to countup

--- components/site/home/countup-numbers.tsx
+++ components/site/home/countup-numbers.tsx
@@ -15,13 +15,14 @@ export default function CountUpNumbers({
   value,
 }: CountUpNumbersProps) {
   return (
-    <div className="flex flex-col items-center justify-between border-4 border-gray-100 w-32 h-40">
+    <div className="flex flex-col items-center justify-between border-4 border-gray-100 w-36 h-40">
       <div className="w-[90px] h-[90px] flex items-center justify-center">
         <Image src={img!} width={60} height={60} alt={`${title} - title`} />
       </div>
       <CountUp
         end={value!}
-        duration={5}
+        delay={1}
+        duration={10}
         className="text-gray-500 font-semibold text-2xl"
       />
       <span className="text-gray-500 font-semibold text-xl">{title}</span>
