d67e21c06ed5a3ed9f4849a5e5fb6469675ecf0b
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-01-17 14:44:58 -0300

    add countup startups, experts and countries

--- app/(site)/[lang]/page.tsx
+++ app/(site)/[lang]/page.tsx
@@ -1,5 +1,12 @@
+import { useTranslations } from "next-intl";
+
+import { expertsList, infoFlags, startupsList } from "@/app/(site)/data";
+import globeImage from "@/assets/img/globe-image.svg";
+import expertImage from "@/assets/img/plans-expert-image.svg";
+import startupImage from "@/assets/img/plans-rocket-image.svg";
 import About from "@/components/site/home/about";
 import Contact from "@/components/site/home/contact";
+import CountUpNumbers from "@/components/site/home/countup-numbers";
 import Hero from "@/components/site/home/hero";
 import OurSolutions from "@/components/site/home/our-solutions";
 import Section from "@/components/site/home/section";
@@ -7,9 +14,28 @@ import Subscription from "@/components/site/home/subscription";
 import WhyChooseUs from "@/components/site/home/why-choose-us";
 
 export default function Home() {
+  const infoFlagsLength = Object.entries(infoFlags).length;
+  const t = useTranslations("Home");
   return (
     <main>
       <Hero />
+      <div className="container grid sm:grid-cols-1 md:grid-cols-3 w-[500px] gap-y-3 place-items-center">
+        <CountUpNumbers
+          title={t("countup-startup-title")}
+          img={startupImage}
+          value={startupsList.length}
+        />
+        <CountUpNumbers
+          title={t("countup-experts-title")}
+          img={expertImage}
+          value={expertsList.length}
+        />
+        <CountUpNumbers
+          title={t("countup-country-title")}
+          img={globeImage}
+          value={infoFlagsLength}
+        />
+      </div>
       <Section>
         <WhyChooseUs />
       </Section>
---
+++ components/site/home/countup-numbers.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import CountUp from "react-countup";
+import Image from "next/image";
+
+interface CountUpNumbersProps {
+  title: string;
+  img: string;
+  value: number;
+}
+
+export default function CountUpNumbers({
+  title,
+  img,
+  value,
+}: CountUpNumbersProps) {
+  return (
+    <div className="flex flex-col items-center justify-between border-4 border-gray-100 w-32 h-40">
+      <div className="w-[90px] h-[90px] flex items-center justify-center">
+        <Image src={img!} width={60} height={60} alt={`${title} - title`} />
+      </div>
+      <CountUp
+        end={value!}
+        duration={5}
+        className="text-gray-500 font-semibold text-2xl"
+      />
+      <span className="text-gray-500 font-semibold text-xl">{title}</span>
+    </div>
+  );
+}
