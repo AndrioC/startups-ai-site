c0928295abbf4c8c5e9aac4417f042c230ae945b
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-01-19 10:15:16 -0300

    remove sgl filter from startups page

--- components/site/startups/header-startups-filter.tsx
+++ components/site/startups/header-startups-filter.tsx
@@ -26,7 +26,6 @@ export default function HeaderStartupsFilter({
   const [country, setCountry] = useState("");
   const [vertical, setVertical] = useState("");
   const [businessModel, setBusinessModel] = useState("");
-  const [sglBadge, setSglBadge] = useState("");
 
   const options = setFilter(dataSource);
 
@@ -34,43 +33,34 @@ export default function HeaderStartupsFilter({
     setCountry("");
     setVertical("");
     setBusinessModel("");
-    setSglBadge("");
     setFilteredData(dataSource);
   }, []);
 
   const handleFilterByCountry = useCallback(
     (country_value: string) => {
-      applyFilters(country_value, vertical, businessModel, sglBadge);
+      applyFilters(country_value, vertical, businessModel);
     },
-    [vertical, businessModel, sglBadge]
+    [vertical, businessModel]
   );
 
   const handleFilterByVertical = useCallback(
     (vertical_value: string) => {
-      applyFilters(country, vertical_value, businessModel, sglBadge);
+      applyFilters(country, vertical_value, businessModel);
     },
-    [country, businessModel, sglBadge]
+    [country, businessModel]
   );
 
   const handleFilterByBusinessModel = useCallback(
     (business_model_value: string) => {
-      applyFilters(country, vertical, business_model_value, sglBadge);
+      applyFilters(country, vertical, business_model_value);
     },
-    [country, vertical, sglBadge]
-  );
-
-  const handleFilterBySglLabel = useCallback(
-    (sgl_badge_value: string) => {
-      applyFilters(country, vertical, businessModel, sgl_badge_value);
-    },
-    [country, vertical, businessModel]
+    [country, vertical]
   );
 
   const applyFilters = (
     country_value: string,
     vertical_value: string,
-    business_model_value: string,
-    sgl_badge_value: string
+    business_model_value: string
   ) => {
     let newFilteredData = [...dataSource];
 
@@ -92,97 +82,74 @@ export default function HeaderStartupsFilter({
       );
     }
 
-    if (sgl_badge_value) {
-      newFilteredData = newFilteredData.filter(
-        (startup) => startup.sgl_badge === sgl_badge_value
-      );
-    }
     setFilteredData(newFilteredData);
   };
 
   return (
-    <div className="flex flex-col gap-3">
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 place-items-center">
-        <Select
-          value={country}
-          onValueChange={(value) => {
-            handleFilterByCountry(value);
-            setCountry(value);
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder={t("header-filter-country")} />
-          </SelectTrigger>
-          <SelectContent>
-            {options[0].map((option, index) => (
-              <SelectItem key={index} value={option.value}>
-                {option.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select
-          value={vertical}
-          onValueChange={(value) => {
-            handleFilterByVertical(value);
-            setVertical(value);
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Vertical" />
-          </SelectTrigger>
-          <SelectContent>
-            {options[1].map((option, index) => (
-              <SelectItem key={index} value={option.value}>
-                {option.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select
-          value={businessModel}
-          onValueChange={(value) => {
-            handleFilterByBusinessModel(value);
-            setBusinessModel(value);
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder={t("header-filter-model-business")} />
-          </SelectTrigger>
-          <SelectContent>
-            {options[2].map((option, index) => (
-              <SelectItem key={index} value={option.value}>
-                {option.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select
-          value={sglBadge}
-          onValueChange={(value) => {
-            handleFilterBySglLabel(value);
-            setSglBadge(value);
-          }}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder={t("header-filter-model-sgl-badge")} />
-          </SelectTrigger>
-          <SelectContent>
-            {options[3].map((option, index) => (
-              <SelectItem key={index} value={option.value}>
-                {option.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      <Button
-        variant="link"
-        onClick={handleClearFilters}
-        className="self-end -mr-3 text-blue-600 font-normal uppercase"
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 place-items-center">
+      <Select
+        value={country}
+        onValueChange={(value) => {
+          handleFilterByCountry(value);
+          setCountry(value);
+        }}
       >
-        {t("header-filter-clear")}
-      </Button>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={t("header-filter-country")} />
+        </SelectTrigger>
+        <SelectContent>
+          {options[0].map((option, index) => (
+            <SelectItem key={index} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select
+        value={vertical}
+        onValueChange={(value) => {
+          handleFilterByVertical(value);
+          setVertical(value);
+        }}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Vertical" />
+        </SelectTrigger>
+        <SelectContent>
+          {options[1].map((option, index) => (
+            <SelectItem key={index} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select
+        value={businessModel}
+        onValueChange={(value) => {
+          handleFilterByBusinessModel(value);
+          setBusinessModel(value);
+        }}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={t("header-filter-model-business")} />
+        </SelectTrigger>
+        <SelectContent>
+          {options[2].map((option, index) => (
+            <SelectItem key={index} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <div>
+        <Button
+          variant="link"
+          onClick={handleClearFilters}
+          className="text-blue-600 font-normal uppercase lg:ml-0 ml-20"
+        >
+          {t("header-filter-clear")}
+        </Button>
+      </div>
     </div>
   );
 }
@@ -241,22 +208,5 @@ function setFilter(dataSource: StartupProps[]) {
       label: value.business_model,
     }));
 
-  const uniqueSglLabel = _.uniqBy(dataSource, "sgl_badge");
-
-  const sglLabelFilter = uniqueSglLabel
-    .sort((a, b) => {
-      if (a.sgl_badge < b.sgl_badge) {
-        return -1;
-      }
-      if (a.sgl_badge > b.sgl_badge) {
-        return 1;
-      }
-      return 0;
-    })
-    .map((value) => ({
-      value: value.sgl_badge,
-      label: value.sgl_badge.charAt(0).toUpperCase() + value.sgl_badge.slice(1),
-    }));
-
-  return [countryFilter, verticalFilter, businessModelFilter, sglLabelFilter];
+  return [countryFilter, verticalFilter, businessModelFilter];
 }
