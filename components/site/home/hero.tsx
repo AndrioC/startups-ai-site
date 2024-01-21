addb8b3816d9a3f177212c2a07cb24bc79e9116b
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-01-21 11:00:28 -0300

    improve lcg hero-bg image

--- components/site/home/hero.tsx
+++ components/site/home/hero.tsx
@@ -53,7 +53,14 @@ export default function Hero() {
           </div>
           <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
             <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
-              <Image src={heroBg} alt="hero-bg" priority />
+              <Image
+                src={heroBg}
+                alt="hero-bg"
+                priority
+                width={600}
+                height={400}
+                loading="lazy"
+              />
             </div>
           </div>
         </div>
