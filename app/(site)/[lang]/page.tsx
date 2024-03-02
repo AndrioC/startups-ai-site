cfff7ea36bf0c57e6787ba66ff0e93e8d7f1d575
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-03-02 17:43:41 -0300

    test

--- app/(site)/[lang]/page.tsx
+++ app/(site)/[lang]/page.tsx
@@ -2,53 +2,32 @@
 
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useLocale, useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 
-import globeImage from "@/assets/img/globe-image.svg";
-import expertImage from "@/assets/img/plans-expert-image.svg";
-import startupImage from "@/assets/img/plans-rocket-image.svg";
+import { InitialCardValues } from "@/app/api/initial-card-values/route";
+import { StartupSummary } from "@/app/api/startups/route";
 import About from "@/components/site/home/about";
 import OurSolutions from "@/components/site/home/about-program";
 import Contact from "@/components/site/home/contact";
-import CountUpNumbers from "@/components/site/home/countup-numbers";
+import CountUpComponent from "@/components/site/home/countup-component";
 import Hero from "@/components/site/home/hero";
 import OurPartners from "@/components/site/home/our-partners";
 import Section from "@/components/site/home/section";
 import Subscription from "@/components/site/home/subscription";
 import WhyChooseUs from "@/components/site/home/why-choose-us";
 
-import { InitialCardValues } from "../../api/initial-card-values/route";
-import { StartupSummary } from "../../api/startups/route";
-
 export default function Home() {
   const lang = useLocale();
-  const t = useTranslations("Home");
 
   console.log("lang", lang);
-
-  const { data: initialCardValuesData } = useInitialCardValues();
-  const { data, isLoading: isLoadingStartups } = useStartups(lang);
-  console.log("HERE: ", initialCardValuesData);
 
   return (
     <main>
       <Hero />
       <div className="container grid sm:grid-cols-1 md:grid-cols-3 w-[300px] md:w-[500px] gap-y-3 place-items-center">
-        <CountUpNumbers
-          title={t("countup-startup-title")}
-          img={startupImage}
-          value={initialCardValuesData?.startups_quantity!}
-        />
-        <CountUpNumbers
-          title={t("countup-experts-title")}
-          img={expertImage}
-          value={initialCardValuesData?.experts_quantity!}
-        />
-        <CountUpNumbers
-          title={t("countup-country-title")}
-          img={globeImage}
-          value={initialCardValuesData?.countries_quantity!}
-        />
+        <div>
+          <CountUpComponent />
+        </div>
       </div>
       <Section>
         <WhyChooseUs />
---
+++ components/site/home/countup-component.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useTranslations } from "next-intl";
+
+import { InitialCardValues } from "@/app/api/initial-card-values/route";
+import globeImage from "@/assets/img/globe-image.svg";
+import expertImage from "@/assets/img/plans-expert-image.svg";
+import startupImage from "@/assets/img/plans-rocket-image.svg";
+
+import CountUpNumbers from "./countup-numbers";
+
+interface Props {
+  data?: InitialCardValues;
+}
+
+export default function CountUpComponent({ data }: Props) {
+  const t = useTranslations("Home");
+
+  const { data: initialCardValuesData } = useInitialCardValues();
+
+  return (
+    <div className="container grid sm:grid-cols-1 md:grid-cols-3 w-[300px] md:w-[500px] gap-y-3 place-items-center">
+      <CountUpNumbers
+        title={t("countup-startup-title")}
+        img={startupImage}
+        value={initialCardValuesData?.startups_quantity!}
+      />
+      <CountUpNumbers
+        title={t("countup-experts-title")}
+        img={expertImage}
+        value={initialCardValuesData?.experts_quantity!}
+      />
+      <CountUpNumbers
+        title={t("countup-country-title")}
+        img={globeImage}
+        value={initialCardValuesData?.countries_quantity!}
+      />
+    </div>
+  );
+}
+
+const useInitialCardValues = () =>
+  useQuery<InitialCardValues>({
+    queryKey: ["initial-card-data"],
+    queryFn: () =>
+      axios.get("/api/initial-card-values").then((res) => {
+        return res.data;
+      }),
+  });
