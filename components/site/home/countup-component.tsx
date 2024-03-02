a33e4da7befa35d4e61367cb156e84d98c28ea89
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-03-02 17:47:48 -0300

    replace axios by fetch

--- components/site/home/countup-component.tsx
+++ components/site/home/countup-component.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 import { InitialCardValues } from "@/app/api/initial-card-values/route";
 import globeImage from "@/assets/img/globe-image.svg";
@@ -15,8 +15,9 @@ interface Props {
 
 export default function CountUpComponent({ data }: Props) {
   const t = useTranslations("Home");
+  const lang = useLocale();
 
-  const { data: initialCardValuesData } = useInitialCardValues();
+  const { data: initialCardValuesData } = useInitialCardValues(lang);
 
   return (
     <div className="container grid sm:grid-cols-1 md:grid-cols-3 w-[300px] md:w-[500px] gap-y-3 place-items-center">
@@ -39,9 +40,9 @@ export default function CountUpComponent({ data }: Props) {
   );
 }
 
-const useInitialCardValues = () =>
+const useInitialCardValues = (lang: string) =>
   useQuery<InitialCardValues>({
-    queryKey: ["initial-card-data"],
+    queryKey: ["initial-card-data", lang],
     queryFn: () =>
       axios.get("/api/initial-card-values").then((res) => {
         return res.data;
